627c964e59330ab977ddf6513c8c95504ee8cc02
Mike He <misaxi@gmail.com>
2013-03-05 13:33:20 +1100

    init

---
+++ webby.py
@@ -0,0 +1,72 @@
+# from subprocess import call
+import subprocess, os
+import sublime, sublime_plugin
+
+def compile(view, file_ext, cmd):
+    fullname = view.file_name()
+    folder, filename = os.path.split(fullname)
+    name, ext = filename.split('.')
+    target = os.path.join(folder, name + '.js')
+    window = view.window()
+    
+    if ext != file_ext:
+        return
+
+    print '%s detected...' % file_ext
+
+    if(window.num_groups() == 1):
+        print 'stop because of single view'
+        return
+
+    cmd = '{0} "{1}"'.format(cmd, fullname)
+    print cmd
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    retval = p.wait()
+    print retval
+    if retval != 0:
+        output = p.stdout.readlines()
+        with open(target, 'w') as f:
+            f.write(''.join(output))
+        f.close()
+
+    window.focus_group(1)
+    window.open_file(target)
+    window.focus_view(view)
+
+class CoffeeScript(sublime_plugin.EventListener):
+    def on_post_save(self, view):
+        compile(view, "coffee", "coffee --compile")
+
+class IcedCoffeeScript(sublime_plugin.EventListener):
+    def on_post_save(self, view):
+        compile(view, "iced", "iced --compile")
+
+class TypeScript(sublime_plugin.EventListener):
+    def on_post_save(self, view):
+        compile(view, "ts", "tsc")
+        # folder, filename = os.path.split(view.file_name())
+        # name, ext = filename.split('.')
+        # target = os.path.join(folder, name + '.js')
+        # window = view.window()
+        
+        # if ext != 'coffee':
+        #     return
+
+        # print 'coffeescript detected...'        
+
+        # if(window.num_groups() == 1):
+        #     print 'stop because of single view'
+        #     return
+
+        # p = subprocess.Popen('coffee --compile "%s"' % view.file_name(), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        # retval = p.wait()
+
+        # if retval != 0:
+        #     output = p.stdout.readlines()
+        #     with open(target, 'w') as f:
+        #         f.write(''.join(output))
+        #     f.close()
+
+        # window.focus_group(1)
+        # window.open_file(target)
+        # window.focus_view(view)
