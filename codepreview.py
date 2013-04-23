04ae01701d8151e5a1cc2a99e4022861d6e8e6c0
Mike He <misaxi@gmail.com>
2013-04-23 16:33:36 +1000

    support for less

--- codepreview.py
+++ codepreview.py
@@ -1,30 +1,30 @@
-# from subprocess import call
 import subprocess, os
 import sublime, sublime_plugin
 
-def compile(view, file_ext, cmd):
-    fullname = view.file_name()
-    folder, filename = os.path.split(fullname)
-    name   = filename[0:filename.rfind('.' + file_ext)]
+def compile(view, source_file_ext, target_file_ext, cmd):
+    source = view.file_name()
+    folder, filename = os.path.split(source)
+    name   = filename[0:filename.rfind('.' + source_file_ext)]
     ext    = filename.rsplit('.',1)[1]
-    target = os.path.join(folder, name + '.js')
+    target = os.path.join(folder, name + '.' + target_file_ext)
     window = view.window()
-    
-    if ext != file_ext:
+
+    if ext != source_file_ext:
         return
 
-    print '[codepreview]', '%s detected...' % file_ext
+    print '[codepreview]', '%s detected...' % source_file_ext
 
     if(window.num_groups() == 1):
         print '[codepreview]', 'stop because of single view'
         return
 
-    cmd = '{0} "{1}"'.format(cmd, fullname)
+    cmd = cmd.replace('{{source}}', source)
+    cmd = cmd.replace('{{target}}', target)
     print '[codepreview]', cmd
     p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     retval = p.wait()
     if retval != 0:
-        print '[codepreview]', 'error occured while compiling the file', fullname
+        print '[codepreview]', 'error occured while compiling the file', source
         output = p.stdout.readlines()
         with open(target, 'w') as f:
             f.write(''.join(output))
@@ -36,38 +36,16 @@ def compile(view, file_ext, cmd):
 
 class CoffeeScript(sublime_plugin.EventListener):
     def on_post_save(self, view):
-        sublime.set_timeout(lambda: compile(view, "coffee", "coffee --compile"), 0)
+        sublime.set_timeout(lambda: compile(view, 'coffee', 'js', 'coffee --compile "{{source}}"'), 0)
 
 class IcedCoffeeScript(sublime_plugin.EventListener):
     def on_post_save(self, view):
-        sublime.set_timeout(lambda: compile(view, "iced", "iced --compile"), 0)
+        sublime.set_timeout(lambda: compile(view, 'iced', 'js', 'iced --compile "{{source}}"'), 0)
         
 class TypeScript(sublime_plugin.EventListener):
     def on_post_save(self, view):
-        sublime.set_timeout(lambda: compile(view, "ts", "tsc"), 0)
-        # folder, filename = os.path.split(view.file_name())
-        # name, ext = filename.split('.')
-        # target = os.path.join(folder, name + '.js')
-        # window = view.window()
-        
-        # if ext != 'coffee':
-        #     return
+        sublime.set_timeout(lambda: compile(view, 'ts', 'js', 'tsc "{{source}}"'), 0)
 
-        # print 'coffeescript detected...'        
-
-        # if(window.num_groups() == 1):
-        #     print 'stop because of single view'
-        #     return
-
-        # p = subprocess.Popen('coffee --compile "%s"' % view.file_name(), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        # retval = p.wait()
-
-        # if retval != 0:
-        #     output = p.stdout.readlines()
-        #     with open(target, 'w') as f:
-        #         f.write(''.join(output))
-        #     f.close()
-
-        # window.focus_group(1)
-        # window.open_file(target)
-        # window.focus_view(view)
+class Less(sublime_plugin.EventListener):
+    def on_post_save(self, view):
+        sublime.set_timeout(lambda: compile(view, 'less', 'css', 'lessc "{{source}}" "{{target}}"'), 0)
